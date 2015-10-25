ed46c812207b8394561a7cd79f47095c81037d08
kagemusha <mmadrid99@gmail.com>
2015-10-25 11:22:37 -0400

    Home page

---
+++ app/routes/index.js
@@ -0,0 +1,12 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  userService: Ember.inject.service(),
+  isLoggedIn: Ember.computed.readOnly("userService.isLoggedIn"),
+
+  beforeModel() {
+    if (this.get('isLoggedIn')){
+      this.transitionTo('home');
+    }
+  },
+});
